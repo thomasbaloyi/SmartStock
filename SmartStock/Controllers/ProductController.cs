940cced1e94b6a2852af300d3e9e880c38b292a5
Thomas Baloyi <thomas.baloyi@pathcare.net>
2023-10-01 19:39:40 +0200

    Display list of products onto table

---
+++ SmartStock/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartStock.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace SmartStock.Controllers
+{
+    public class ProductController : Controller
+    {
+        private const string BaseUrl = "https://gendacproficiencytest.azurewebsites.net/API/";
+
+        // GET: ProductController
+        public async Task<IActionResult> Index()
+        {
+            List<ProductModel> products = new List<ProductModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("ProductsAPI");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    products = JsonSerializer.Deserialize<List<ProductModel>>(content);
+                }
+                else
+                {
+                    // Handle the error appropriately
+                    ModelState.AddModelError(string.Empty, "Error retrieving data from the API.");
+                }
+            }
+
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SmartStock/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartStock.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
