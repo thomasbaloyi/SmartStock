0ef8050f9d131bcc9fdadc7ef7eb130bf92fbcfa
Thomas Baloyi <thomas.baloyi@pathcare.net>
2023-10-01 18:48:35 +0200

    Initial commit

---
+++ SmartStock/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartStock.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
